0d3fdb5744e2ecb7a00ff509d25fb500367f6fe8
Wasif Fahim <wasif.fahim@gmail.com>
2020-07-01 22:56:16 -0700

    Implemented lists

--- main.py
+++ main.py
@@ -11,4 +11,15 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
-print(data)
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        docs.append(pattern)
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
